dbf0c41760d16bfd775045c4ff46f1975b46b0d1
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2024-09-11 23:33:57 +0300

    Better indev driver

--- lv_port_mre/lv_port_indev_mre.c
+++ lv_port_mre/lv_port_indev_mre.c
@@ -3,7 +3,6 @@
  *********************/
 #include "lv_port_indev_mre.h"
 #include "vmio.h"
-#include "event_loop.h"
 
 /*********************
  *      DEFINES
@@ -25,8 +24,8 @@ static lv_indev_state_t touchpad_event_mapping(uint8_t event);
 static void keypad_init(void);
 static void keypad_read(lv_indev_t* indev, lv_indev_data_t* data);
 static void keyboard_event(VMINT event, VMINT keycode);
-static lv_indev_state_t keyboard_event_mapping(uint8_t event);
-static uint32_t keyboard_key_mapping(uint8_t key);
+static lv_indev_state_t keyboard_event_mapping(int8_t event);
+static uint32_t keyboard_key_mapping(int8_t key);
 
 /**********************
  *  STATIC VARIABLES
@@ -34,8 +33,12 @@ static uint32_t keyboard_key_mapping(uint8_t key);
 static lv_indev_t* indev_touchpad;
 static lv_indev_t* indev_keypad;
 
-static struct touchpad_event_loop_t touchpad_event_loop;
-static struct keyboard_event_loop_t keyboard_event_loop;
+static int16_t touchpad_x = 0;
+static int16_t touchpad_y = 0;
+static lv_indev_state_t touchpad_state = 0;
+
+static lv_indev_state_t keyboard_state = 0;
+static int32_t keyboard_key = 0;
 
 
 /**********************
@@ -59,6 +62,7 @@ void lv_port_indev_init(void)
 	indev_touchpad = lv_indev_create();
 	lv_indev_set_type(indev_touchpad, LV_INDEV_TYPE_POINTER);
 	lv_indev_set_read_cb(indev_touchpad, touchpad_read);
+	lv_indev_set_mode(indev_touchpad, LV_INDEV_MODE_EVENT);
 
 	/*------------------
 	 * Keypad
@@ -71,6 +75,7 @@ void lv_port_indev_init(void)
 	indev_keypad = lv_indev_create();
 	lv_indev_set_type(indev_keypad, LV_INDEV_TYPE_KEYPAD);
 	lv_indev_set_read_cb(indev_keypad, keypad_read);
+	lv_indev_set_mode(indev_keypad, LV_INDEV_MODE_EVENT);
 
 	/*Later you should create group(s) with `lv_group_t * group = lv_group_create()`,
 	 *add objects to the group with `lv_group_add_obj(group, obj)`
@@ -91,29 +96,23 @@ void lv_port_indev_init(void)
 static void touchpad_init(void)
 {
 	vm_reg_pen_callback(touchpad_event_handler);
-	touchpad_event_loop_init(&touchpad_event_loop);
 }
 
 /*Will be called by the library to read the touchpad*/
 static void touchpad_read(lv_indev_t* indev_drv, lv_indev_data_t* data)
 {
-	struct touchpad_event_t ev = touchpad_event_loop_pop(&touchpad_event_loop);
-
-	data->state = touchpad_event_mapping(ev.event);
-	data->point.x = ev.x;
-	data->point.y = ev.y;
-	data->continue_reading = touchpad_event_loop_is_available(&touchpad_event_loop);
+	data->state = touchpad_state;
+	data->point.x = touchpad_x;
+	data->point.y = touchpad_y;
 }
 
 static void touchpad_event_handler(VMINT event, VMINT x, VMINT y) 
 {
-	struct touchpad_event_t ev;
+	touchpad_x = x;
+	touchpad_y = y;
+	touchpad_state = touchpad_event_mapping(event);
 
-	ev.event = event;
-	ev.x = x;
-	ev.y = y;
-
-	touchpad_event_loop_push(&touchpad_event_loop, ev);
+	lv_indev_read(indev_touchpad);
 }
 
 static lv_indev_state_t touchpad_event_mapping(uint8_t event) {
@@ -142,29 +141,25 @@ static lv_indev_state_t touchpad_event_mapping(uint8_t event) {
 static void keypad_init(void)
 {
 	vm_reg_keyboard_callback(keyboard_event);
-	keyboard_event_loop_init(&keyboard_event_loop);
 }
 
 /*Will be called by the library to read the mouse*/ // What?
 static void keypad_read(lv_indev_t* indev_drv, lv_indev_data_t* data)
 {
-	struct keyboard_event_t ev = keyboard_event_loop_pop(&keyboard_event_loop);
-
-	data->state = keyboard_event_mapping(ev.event);
-	data->key = keyboard_key_mapping(ev.keycode);
-	data->continue_reading = keyboard_event_loop_is_available(&keyboard_event_loop);
+	data->state = keyboard_state;
+	data->key = keyboard_key;
 }
 
-static void keyboard_event(VMINT event, VMINT keycode) {
-	struct keyboard_event_t ev;
+static void keyboard_event(VMINT event, VMINT keycode) 
+{
+	keyboard_state = keyboard_event_mapping(event);
+	keyboard_key = keyboard_key_mapping(keycode);
 
-	ev.event = event;
-	ev.keycode = keycode;
-
-	keyboard_event_loop_push(&keyboard_event_loop, ev);
+	lv_indev_read(indev_keypad);
 }
 
-static lv_indev_state_t keyboard_event_mapping(int8_t event) {
+static lv_indev_state_t keyboard_event_mapping(int8_t event) 
+{
 	switch (event) {
 	case VM_KEY_EVENT_DOWN:
 	case VM_KEY_EVENT_LONG_PRESS:
@@ -177,7 +172,8 @@ static lv_indev_state_t keyboard_event_mapping(int8_t event) {
 	}
 }
 
-static uint32_t keyboard_key_mapping(int8_t key) {
+static uint32_t keyboard_key_mapping(int8_t key) 
+{
 	switch (key) {
 	case VM_KEY_UP:
 		return LV_KEY_UP;
